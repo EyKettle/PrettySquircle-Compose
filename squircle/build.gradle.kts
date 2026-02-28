271a3fbabc505426f2dbc112ce7d65f8f14a7694
eykettle <eykettle@163.com>
2026-02-28 15:19:52 +0800

    fix(gradle): Specify JVM version 17

--- squircle/build.gradle.kts
+++ squircle/build.gradle.kts
@@ -24,6 +24,10 @@ android {
             )
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
